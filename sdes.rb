651caab800d144d30c085f5378d321faaf377359
JOogway <rut1900@o2.pl>
2015-11-02 22:53:45 +0100

    dafuq

--- sdes.rb
+++ sdes.rb
@@ -1,10 +1,15 @@
 class SDES
+  s0 = []
+  s1 = []
+  pw = []
+  p4 = []
   w = [1,1,1,1,0,0,0]
   p10 = []
+  xor3 = []
   if w.length < 8
     w.push(0)
   end
-  def P10w8 tab1, tab2
+  def p10w8 tab1, tab2
     tab1.push(tab2[5])
     tab1.push(tab2[2])
     tab1.push(tab2[6])
@@ -25,10 +30,131 @@ class SDES
     tab2.push(tab1[8])
     tab2.push(tab1[7])
     tab2.push(tab1[5])
-
   end
-  def p4
-    
+
+  def p4 tab1,tab2, p4
+    p4.push(tab1[1])
+    p4.push(tab2[1])
+    p4.push(tab2[0])
+    p4.push(tab1[0])
+  end
+  def pw w, pw
+  pw.push(w[1])
+  pw.push(w[5])
+  pw.push(w[2])
+  pw.push(w[0])
+  pw.push(w[3])
+  pw.push(w[7])
+  pw.push(w[4])
+  pw.push(w[6])
+  end
+  def sbox1 tab , s0
+    puts case tab
+           when "[0, 0, 0, 0]"
+             s0.push(0)
+             s0.push(1)
+           when "[0, 0, 1, 0]"
+             s0.push(0)
+             s0.push(0)
+           when "[0, 1, 0, 0]"
+             s0.push(1)
+             s0.push(1)
+           when "[0, 1, 1, 0]"
+             s0.push(1)
+             s0.push(0)
+           when "[0, 0, 0, 1]"
+             s0.push(1)
+             s0.push(1)
+           when "[0, 0, 1, 1]"
+             s0.push(1)
+             s0.push(0)
+           when "[0, 1, 0, 1]"
+             s0.push(0)
+             s0.push(1)
+           when "[0, 1, 1, 1]"
+             s0.push(0)
+             s0.push(0)
+           when "[1, 0, 0, 0]"
+             s0.push(0)
+             s0.push(0)
+           when "[1, 0, 1, 0]"
+             s0.push(0)
+             s0.push(1)
+           when "[1, 1, 0, 0]"
+             s0.push(0)
+             s0.push(1)
+           when "[1, 1, 1, 0]"
+             s0.push(1)
+             s0.push(1)
+           when "[1, 0, 0, 1]"
+             s0.push(1)
+             s0.push(1)
+           when "[1, 0, 1, 1]"
+             s0.push(0)
+             s0.push(1)
+           when "[1, 1, 0, 1]"
+             s0.push(1)
+             s0.push(1)
+           when "[1, 1, 1, 1]"
+             s0.push(1)
+             s0.push(0)
+           else
+             puts "ni ma"
+         end
+  end
+  def sbox2 tab , s0
+    puts case tab
+           when "[0, 0, 0, 0]"
+             s0.push(0)
+             s0.push(0)
+           when "[0, 0, 0, 1]"
+             s0.push(1)
+             s0.push(0)
+           when "[0, 0, 1, 0]"
+             s0.push(0)
+             s0.push(1)
+           when "[0, 0, 1, 1]"
+             s0.push(0)
+             s0.push(0)
+           when "[0, 1, 0, 0]"
+             s0.push(1)
+             s0.push(0)
+           when "[0, 1, 0, 1]"
+             s0.push(0)
+             s0.push(1)
+           when "[0, 1, 1, 0]"
+             s0.push(1)
+             s0.push(1)
+           when "[0, 1, 1, 1]"
+             s0.push(1)
+             s0.push(1)
+           when "[1, 0, 0, 0]"
+             s0.push(1)
+             s0.push(1)
+           when "[1, 0, 0, 1]"
+             s0.push(1)
+             s0.push(0)
+           when "[1, 0, 1, 0]"
+             s0.push(0)
+             s0.push(0)
+           when "[1, 0, 1, 1]"
+             s0.push(0)
+             s0.push(1)
+           when "[1, 1, 0, 0]"
+             s0.push(0)
+             s0.push(1)
+           when "[1, 1, 0, 1]"
+             s0.push(0)
+             s0.push(0)
+           when "[1, 1, 1, 0]"
+             s0.push(0)
+             s0.push(0)
+           when "[1, 1, 1, 1]"
+             s0.push(1)
+             s0.push(1)
+           else
+             puts "ni ma"
+         end
   end
   f = SDES.new
   #puts w.inspect
@@ -52,7 +178,7 @@ class SDES
   puts "SL1 : #{sl1.inspect}"
 
   p10w8 = []
-  f.P10w8 p10w8, sl1
+  f.p10w8 p10w8, sl1
 
   k1 = p10w8
   puts "Klucz pierwszej rundy #{k1.inspect}"
@@ -70,7 +196,7 @@ class SDES
   puts "SL2: #{sl2.inspect}"
 
   p10w8i = []
-  f.P10w8 p10w8i,sl2
+  f.p10w8 p10w8i,sl2
 
   k2 = p10w8i
   puts "Klucz drugiej rundy #{k2.inspect}"
@@ -110,7 +236,17 @@ class SDES
   puts "Pierwsza polowa xor #{xor1.inspect}"
   puts "Druga polowa xor #{xor2.inspect}"
 
+  f.sbox1 xor1.to_s, s0
+  f.sbox2 xor2.to_s, s1
 
-
-
+  puts s0.inspect
+  f.p4 s0,s1,p4
+  puts p4.inspect
+  for i in 0..3
+    xor3[i]=(t[i].to_i+p4[i].to_i)%2
+  end
+  xortp4= xor3[0..3]+w[3..6]
+  puts xortp4.inspect
+  f.pw w, pw
+  puts "PW: #{pw.inspect}"
 end
